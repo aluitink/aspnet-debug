f0d2ff485cea5e5bbb82b4697a8147a3234f10b5
habathcx <aluitink@gmail.com>
2016-03-12 19:32:53 -0700

    switching from console write to logger

--- src/aspnet-debug.Shared/Server/ClientSession.cs
+++ src/aspnet-debug.Shared/Server/ClientSession.cs
@@ -87,17 +87,17 @@ namespace aspnet_debug.Shared.Server
                                 process.StartInfo = startInfo;
                                 process.OutputDataReceived += (sender, args) =>
                                 {
-                                    Console.WriteLine(args.Data);
+                                    _logger.DebugFormat(args.Data);
                                     stringBuilder.Append(args.Data);
                                 };
                                 process.Start();
 
-                                Console.WriteLine("Process running: {0}", !process.HasExited);
+                                _logger.DebugFormat("Process running: {0}", !process.HasExited);
 
                                 process.WaitForExit();
 
-                                Console.WriteLine("DNX");
-                                Console.WriteLine(stringBuilder.ToString());
+                                _logger.DebugFormat("DNX");
+                                _logger.DebugFormat(stringBuilder.ToString());
                             }
 
                             break;
